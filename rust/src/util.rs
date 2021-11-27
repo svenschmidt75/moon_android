22d96f9e746c5c775074ee8b07413140c39846ac
Sven <svenschmidt75@googlemail.com>
2021-11-27 14:56:51 -0700

    fix signs for perturbation terms

--- rust/src/util.rs
+++ rust/src/util.rs
@@ -10,10 +10,16 @@ pub fn map_to_0_to_360(angle: f64) -> f64 {
 }
 
 const DEGREES_TO_RADIANS: f64  = std::f64::consts::PI / 180.0;
+const RADIANS_TO_DEGREES: f64  = 1.0 / DEGREES_TO_RADIANS;
 
 // Convert from degrees [0, 360) to [0, 2 pi)
 pub fn to_radians(angle: f64) -> f64 {
     angle * DEGREES_TO_RADIANS
+}
+
+// Convert from radians [0, 2 pi) to [0, 360)
+pub fn to_degrees(angle: f64) -> f64 {
+    angle * RADIANS_TO_DEGREES
 }
 
 #[cfg(test)]
