a77576c5bf1ed4109cd2a7b922c524a9ef11e01a
Sven <svenschmidt75@googlemail.com>
2021-12-31 10:28:44 -0700

    initial navigation drawer

---
+++ Kitana/app/src/main/java/com/svenschmidt/kitana/MainContent.kt
@@ -0,0 +1,11 @@
+package com.svenschmidt.kitana
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainContent : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_content)
+    }
+}
