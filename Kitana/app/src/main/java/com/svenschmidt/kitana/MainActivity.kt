ae4c39e1a78fc5d1d699ac4dcda210d702017aa5
Sven <svenschmidt75@googlemail.com>
2022-01-02 08:03:50 -0700

    wire-up set date time menu item

--- Kitana/app/src/main/java/com/svenschmidt/kitana/MainActivity.kt
+++ Kitana/app/src/main/java/com/svenschmidt/kitana/MainActivity.kt
@@ -1,17 +1,22 @@
 package com.svenschmidt.kitana
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.core.view.GravityCompat
+import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         setupActionBar()
+
+        drawer_main_view.setNavigationItemSelectedListener(this)
     }
 
     private fun setupActionBar() {
@@ -37,6 +42,18 @@ class MainActivity : AppCompatActivity() {
         } else {
             main_drawer_layout.openDrawer(GravityCompat.START)
         }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.date_time -> {
+                var intent = Intent(this, DateTimeActivity::class.java)
+                startActivity(intent)
+            }
+        }
+
+        main_drawer_layout.closeDrawer(GravityCompat.START)
+        return true
     }
 
 }
