d1971cd22632c68f1d63f949d2af8be6ee35e8f8
Sven <svenschmidt75@googlemail.com>
2022-01-02 06:35:55 -0700

    add custom action bar and chose theme without action bar

--- Kitana/app/src/main/java/com/svenschmidt/kitana/MainActivity.kt
+++ Kitana/app/src/main/java/com/svenschmidt/kitana/MainActivity.kt
@@ -2,10 +2,35 @@ package com.svenschmidt.kitana
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.view.GravityCompat
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.app_bar_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupActionBar()
     }
+
+    private fun setupActionBar() {
+
+        setSupportActionBar(toolbar_main_activity)
+        toolbar_main_activity.setNavigationIcon(R.drawable.ic_action_navigation_menu)
+
+        toolbar_main_activity.setNavigationOnClickListener {
+            toggleDrawer()
+        }
+    }
+
+    private fun toggleDrawer() {
+
+        if (main_drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            main_drawer_layout.closeDrawer(GravityCompat.START)
+        } else {
+            main_drawer_layout.openDrawer(GravityCompat.START)
+        }
+    }
+
 }
