e56ab2cf2e1af75aeb380aa053dc60014f40bdf3
Sven <svenschmidt75@googlemail.com>
2022-01-02 08:12:14 -0700

    add viewmodel for SetDateTimeActivity

--- Kitana/app/src/main/java/com/svenschmidt/kitana/DateTimeActivity.kt
+++ Kitana/app/src/main/java/com/svenschmidt/kitana/DateTimeActivity.kt
@@ -1,11 +1,22 @@
 package com.svenschmidt.kitana
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.AttributeSet
+import android.view.View
+import androidx.lifecycle.ViewModelProvider
+import com.svenschmidt.kitana.viewmodel.DateTimeViewModel
 
 class DateTimeActivity : AppCompatActivity() {
+
+    lateinit var viewModel: DateTimeViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_date_time)
+
+        viewModel = ViewModelProvider(this).get(DateTimeViewModel::class.java)
     }
+
 }
---
+++ Kitana/app/src/main/java/com/svenschmidt/kitana/viewmodel/DateTimeViewModel.kt
@@ -0,0 +1,8 @@
+package com.svenschmidt.kitana.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class DateTimeViewModel(application: Application): AndroidViewModel(application) {
+
+}
