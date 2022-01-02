6c2d8ace430e68044e8d6a1721d23fc5cb03f0b8
Sven <svenschmidt75@googlemail.com>
2022-01-02 09:39:06 -0700

    minor

--- Kitana/core/src/main/java/com/svenschmidt/core/model/DateTimeProvider.kt
+++ Kitana/core/src/main/java/com/svenschmidt/core/model/DateTimeProvider.kt
@@ -1,4 +1,18 @@
 package com.svenschmidt.core.model
 
+import java.util.*
+
+
 class DateTimeProvider {
+
+    inner class Helper: TimerTask() {
+        override fun run() {
+
+        }
+    }
+
+    fun start(period: Long) {
+        val timer = Timer()
+
+    }
 }
