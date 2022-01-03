7a71ab43a98e8ed85a9f3c7a99419b9c33b2ffb2
Sven <svenschmidt75@googlemail.com>
2022-01-03 08:06:52 -0700

    add data binding to DateTimeViewModel

--- Kitana/app/src/main/java/com/svenschmidt/kitana/DateTimeActivity.kt
+++ Kitana/app/src/main/java/com/svenschmidt/kitana/DateTimeActivity.kt
@@ -1,20 +1,21 @@
 package com.svenschmidt.kitana
 
-import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.AttributeSet
-import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
+import com.svenschmidt.kitana.databinding.ActivityDateTimeBinding
 import com.svenschmidt.kitana.viewmodel.DateTimeViewModel
 
 class DateTimeActivity : AppCompatActivity() {
 
     lateinit var viewModel: DateTimeViewModel
+    lateinit var binding: ActivityDateTimeBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_date_time)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_date_time)
+        binding.lifecycleOwner = this
 
         viewModel = ViewModelProvider(this).get(DateTimeViewModel::class.java)
     }
--- Kitana/app/src/main/java/com/svenschmidt/kitana/viewmodel/DateTimeViewModel.kt
+++ Kitana/app/src/main/java/com/svenschmidt/kitana/viewmodel/DateTimeViewModel.kt
@@ -2,6 +2,8 @@ package com.svenschmidt.kitana.viewmodel
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 import com.svenschmidt.kitana.core.DateTimeProvider
 import com.svenschmidt.kitana.di.DaggerViewModelComponent
 import javax.inject.Inject
@@ -11,7 +13,12 @@ class DateTimeViewModel(application: Application): AndroidViewModel(application)
     @Inject
     lateinit var dateTimeProvider: DateTimeProvider
 
+    val useCurrentTime = MutableLiveData<Boolean>()
+
     init {
         DaggerViewModelComponent.builder().build().inject(this)
     }
+
+
+
 }
