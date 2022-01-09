e1c2140ddf6eae33815bfcdf7072cf987bb58f7d
Sven <svenschmidt75@googlemail.com>
2022-01-09 07:16:37 -0700

    add moon animation viewmodel and toolbar

--- Kitana/app/src/main/java/com/svenschmidt/kitana/activities/MoonAnimationActivity.kt
+++ Kitana/app/src/main/java/com/svenschmidt/kitana/activities/MoonAnimationActivity.kt
@@ -2,11 +2,37 @@ package com.svenschmidt.kitana.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import com.svenschmidt.kitana.R
+import com.svenschmidt.kitana.databinding.ActivityMoonAnimationBinding
+import com.svenschmidt.kitana.databinding.ActivityMoonBinding
+import com.svenschmidt.kitana.viewmodel.MoonActivityViewModel
+import com.svenschmidt.kitana.viewmodel.MoonAnimationActivityViewModel
+import kotlinx.android.synthetic.main.activity_moon.*
+import kotlinx.android.synthetic.main.activity_moon_animation.*
 
 class MoonAnimationActivity : AppCompatActivity() {
+    lateinit var viewModel: MoonAnimationActivityViewModel
+    lateinit var binding: ActivityMoonAnimationBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_moon_animation)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_moon_animation)
+        binding.lifecycleOwner = this
+
+        viewModel = ViewModelProvider(this).get(MoonAnimationActivityViewModel::class.java)
+        binding.viewmodel = viewModel
+
+        setupActionBar()
+    }
+
+    private fun setupActionBar() {
+
+        setSupportActionBar(moon_animation_activity_toolbar)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        moon_animation_activity_toolbar.setNavigationOnClickListener {
+            onBackPressed()
+        }
     }
 }
---
+++ Kitana/app/src/main/java/com/svenschmidt/kitana/viewmodel/MoonAnimationActivityViewModel.kt
@@ -0,0 +1,7 @@
+package com.svenschmidt.kitana.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class MoonAnimationActivityViewModel(application: Application) : AndroidViewModel(application) {
+}
