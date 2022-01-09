448fd1dbdd6fe792f017c3b09b3b5fbac4db6016
Sven <svenschmidt75@googlemail.com>
2022-01-09 07:13:50 -0700

    add toolbar to moon activity, add viewmodel

--- Kitana/app/src/main/java/com/svenschmidt/kitana/activities/MoonActivity.kt
+++ Kitana/app/src/main/java/com/svenschmidt/kitana/activities/MoonActivity.kt
@@ -1,12 +1,37 @@
 package com.svenschmidt.kitana.activities
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import com.svenschmidt.kitana.R
+import com.svenschmidt.kitana.databinding.ActivityMoonBinding
+import com.svenschmidt.kitana.viewmodel.MoonActivityViewModel
+import kotlinx.android.synthetic.main.activity_moon.*
 
 class MoonActivity : AppCompatActivity() {
+
+    lateinit var viewModel: MoonActivityViewModel
+    lateinit var binding: ActivityMoonBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_moon)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_moon)
+        binding.lifecycleOwner = this
+
+        viewModel = ViewModelProvider(this).get(MoonActivityViewModel::class.java)
+        binding.viewmodel = viewModel
+
+        setupActionBar()
     }
+
+    private fun setupActionBar() {
+
+        setSupportActionBar(moon_activity_toolbar)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        moon_activity_toolbar.setNavigationOnClickListener {
+            onBackPressed()
+        }
+    }
+
 }
---
+++ Kitana/app/src/main/java/com/svenschmidt/kitana/viewmodel/MoonActivityViewModel.kt
@@ -0,0 +1,7 @@
+package com.svenschmidt.kitana.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class MoonActivityViewModel(application: Application) : AndroidViewModel(application) {
+}
