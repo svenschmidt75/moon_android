940ef658c5acd6db88de19f2debb4f4b8e844ca5
Sven <svenschmidt75@googlemail.com>
2022-01-15 13:45:14 -0700

    layout for moon activity

--- Kitana/app/src/main/java/com/svenschmidt/kitana/viewmodel/MoonActivityViewModel.kt
+++ Kitana/app/src/main/java/com/svenschmidt/kitana/viewmodel/MoonActivityViewModel.kt
@@ -2,6 +2,43 @@ package com.svenschmidt.kitana.viewmodel
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import com.svenschmidt.kitana.core.DateTimeProvider
+import javax.inject.Inject
 
 class MoonActivityViewModel(application: Application) : AndroidViewModel(application) {
+
+    @Inject
+    lateinit var dateTimeProvider: DateTimeProvider
+
+    val fractionIlluminated = MutableLiveData<String>()
+    val phaseName = MutableLiveData<String>()
+    val geocentricLongitude = MutableLiveData<String>()
+    val geocentricLatitude = MutableLiveData<String>()
+    val distance = MutableLiveData<String>()
+    val rightAscension = MutableLiveData<String>()
+    val declination = MutableLiveData<String>()
+    val altitude = MutableLiveData<String>()
+    val azimuth = MutableLiveData<String>()
+    val rises = MutableLiveData<String>()
+    val transits = MutableLiveData<String>()
+    val sets = MutableLiveData<String>()
+
+
+    init {
+        fractionIlluminated.postValue("56.4%")
+        phaseName.postValue("Waning Crescent")
+        geocentricLongitude.postValue("175.365")
+        geocentricLatitude.postValue("75.365")
+        distance.postValue("56.4%")
+        rightAscension.postValue("Waning Crescent")
+        declination.postValue("175.365")
+        altitude.postValue("75.365")
+        azimuth.postValue("56.4%")
+        rises.postValue("Waning Crescent")
+        transits.postValue("175.365")
+        sets.postValue("75.365")
+    }
+
+
 }
--- Kitana/app/src/test/java/com/svenschmidt/kitana/ExampleUnitTest.kt
+++ Kitana/app/src/test/java/com/svenschmidt/kitana/ExampleUnitTest.kt
@@ -1,17 +1,24 @@
 package com.svenschmidt.kitana
 
+import android.app.Application
+import com.svenschmidt.kitana.viewmodel.DateTimeViewModel
 import org.junit.Test
-
 import org.junit.Assert.*
+import org.mockito.Mockito
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-class ExampleUnitTest {
+class DateTimeViewModelTest {
+
     @Test
     fun addition_isCorrect() {
+        // Arrange
+        val applicationMock = Mockito.mock(Application::class.java)
+        val model = DateTimeViewModel(applicationMock)
+
         assertEquals(4, 2 + 2)
     }
 }
